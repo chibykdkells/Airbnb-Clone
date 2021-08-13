5986c656e443b8f463c7b121a213460e8d76ec4e
chibykdkells <chigbatachibuike@gmail.com>
2021-08-13 23:01:14 +0100

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+function Footer() {
+    return (
+       <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-800">           
+            <div className="space-y-4 text-xs text-gray-800">
+                 <h5 className="font-bold">ABOUT</h5>
+                 <p>How Airbnb Works</p>
+                 <p>NewsRoom</p>
+                 <p>Investors</p>
+                 <p>Airbnb Plus</p>
+                 <p>Airbnb Luxe</p>   
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                 <h5 className="font-bold">COMMUNITY</h5>
+                 <p>Accessibility</p>
+                 <p>This is not a real site</p>
+                 <p>Its a pretty awesome clone</p>
+                 <p>Lets build your software</p>
+                 <p>Kelvin's Potfolio</p>
+            </div>
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                 <h5 className="font-bold">Projects Completed</h5>
+                 <p>ArewaTechub</p>
+                 <p>Assist Ng</p>
+                 <p>Airbnb Clone</p>
+                 <p>Facebook Clone</p>
+                 <p>Speed Power Clone </p>
+            </div>
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                 <h5 className="font-bold">I Reapted this</h5>
+                 <p>Accessibility</p>
+                 <p>This is not a real site</p>
+                 <p>Its a pretty awesome clone</p>
+                 <p>Lets build your software</p>
+                 <p>Kelvin's Potfolio</p> 
+            </div>         
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                 <h5 className="font-bold"></h5>
+                 
+            </div>
+       </div>
+    )
+}
+
+export default Footer
